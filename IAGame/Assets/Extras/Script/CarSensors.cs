633b06e21dfa4994c35d9cb2138eb4d63dbc338b
Joan Carcaboso Ginard <joan@Joans-MacBook-Pro.local>
2018-05-22 23:09:43 +0200

    Sensors changes

--- IAGame/Assets/Extras/Script/CarSensors.cs
+++ IAGame/Assets/Extras/Script/CarSensors.cs
@@ -1,72 +1,65 @@
-﻿    using System.Collections;
-    using System.Collections.Generic;
-    using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-    public class CarSensors : MonoBehaviour {
+public class CarSensors : MonoBehaviour
+{
 
-    		public float sensor_length, angle_sensors;
+	public float sensor_length, angle_sensors;
 
-    		private const float HALF_CAR = 2.25f;
+	private const float HALF_HEIGTH_CAR = 4.7f, HALF_WIDTH_CAR = 0.9f;
 
-        		//GameObject car;
-    		bool show_sensors = true;
+	//GameObject car;
+	bool show_sensors = true;
 
-    		private void Awake () {
-    		    //car.transform = this.gameObject; 
-    		}
+	void FixedUpdate()
+	{
 
-    		void FixedUpdate () {
+		if (Input.GetKeyUp(KeyCode.I))
+		{
+			show_sensors = !show_sensors;
+		}
+		Sensors(this.transform);
+	}
 
-    			if (Input.GetKeyUp (KeyCode.I)) {
-    				show_sensors = !show_sensors;
-    			}
-    		Sensors (this.transform);
-    		}
+	void Sensors(Transform car)
+	{
+		RaycastHit hit;
+		Vector3 sensor_pos; 
 
-    	    void Sensors (Transform car) {
-    			RaycastHit hit;
-    		    Vector3 sensor_start_pos = car.position; 
-            
-    			//sensor_start_pos.z += HALF_CAR;
-    			//sensor_start_pos.y += 1;
+		sensor_pos = car.position;
+		sensor_pos += car.forward * HALF_HEIGTH_CAR; 
 
-    			//Front
-    			if (Physics.Raycast (sensor_start_pos, car.transform.forward, out hit, sensor_length)) {
-    				if (show_sensors) {
-    					Debug.DrawLine (sensor_start_pos, hit.point);
-    				}
-    			} 
-            
-    			sensor_start_pos.x += -3f;
-    			//Right
-    			if (Physics.Raycast (sensor_start_pos, car.transform.forward, out hit, sensor_length)) {
-    				if (show_sensors) {
-    					Debug.DrawLine (sensor_start_pos, hit.point);
-    				}
-    			}
-    			
-    			//Right angle
-    			if (Physics.Raycast (sensor_start_pos, Quaternion.AngleAxis (angle_sensors, car.transform.up) * car.transform.forward, out hit, sensor_length)) {
-    				if (show_sensors) {
-    					Debug.DrawLine (sensor_start_pos, hit.point);
-    				}
-    			}
+		//Front
+		if (Physics.Raycast(sensor_pos, car.transform.forward, out hit, sensor_length))
+		{
+			if (show_sensors)
+			{
+				Debug.DrawLine(sensor_pos, hit.point, Color.red);
+			}
+		}
 
-    			//sensor_start_pos.x += 3f;
-    		    //sensor_start_pos.z -= 3f;
-    			////Left
-    			//if (Physics.Raycast (sensor_start_pos, car.transform.forward, out hit, sensor_length)) {
-    			//	if (show_sensors) {
-    			//		Debug.DrawLine (sensor_start_pos, hit.point);
-    			//	}
-    			//}
+		sensor_pos += car.right * HALF_WIDTH_CAR; 
+		//Right
+		if (Physics.Raycast(sensor_pos, 
+		                    Quaternion.AngleAxis(angle_sensors, car.transform.up) * car.transform.forward, out hit, sensor_length) )
+		{
+			if (show_sensors)
+			{
+				Debug.DrawLine(sensor_pos, hit.point, Color.yellow);
+			}
+		}
 
-    			////Left angle
-    			//if (Physics.Raycast (sensor_start_pos, Quaternion.AngleAxis (-angle_sensors, car.transform.up) * car.transform.forward, out hit, sensor_length)) {
-    			//	if (show_sensors) {
-    			//		Debug.DrawLine (sensor_start_pos, hit.point);
-    			//	}
-    			//} 
-    		}
-    	
-    }
+		sensor_pos += car.right * (-2f * HALF_WIDTH_CAR ); 
+		//Left
+		if (Physics.Raycast(sensor_pos, 
+		                    Quaternion.AngleAxis(-angle_sensors, car.transform.up) * car.transform.forward, out hit, sensor_length))
+		{
+			if (show_sensors)
+			{
+				Debug.DrawLine(sensor_pos, hit.point, Color.blue);
+			}
+		}
+	}
+
+}
