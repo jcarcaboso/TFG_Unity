d569cb5a46cdbe66a0bcef489aba6bbd1bb0f1e9
Joan Carcaboso Ginard <joan@Joans-MacBook-Pro.local>
2018-05-26 20:54:55 +0200

    Car movement

--- IAGame/Assets/Extras/Script/CarSensors.cs
+++ IAGame/Assets/Extras/Script/CarSensors.cs
@@ -2,64 +2,77 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CarSensors : MonoBehaviour
-{
-
-	public float sensor_length, angle_sensors;
-
-	private const float HALF_HEIGTH_CAR = 4.7f, HALF_WIDTH_CAR = 0.9f;
-
-	//GameObject car;
-	bool show_sensors = true;
-
-	void FixedUpdate()
+namespace UnityStandardAssets.Vehicles.Car
+{	
+	[RequireComponent(typeof(CarController))]
+	public class CarSensors : MonoBehaviour
 	{
+        
+		public float sensor_length, angle_sensors;
 
-		if (Input.GetKeyUp(KeyCode.I))
+		private const float HALF_HEIGTH_CAR = 4.7f, HALF_WIDTH_CAR = 0.9f;
+
+		bool show_sensors = true;
+		CarMovement movement; 
+
+	    void Awake()
 		{
-			show_sensors = !show_sensors;
+			movement = new CarMovement(GetComponent<CarController>());
 		}
-		Sensors(this.transform);
+
+		void FixedUpdate()
+		{
+
+			if (Input.GetKeyUp(KeyCode.I))
+			{
+				show_sensors = !show_sensors;
+			}
+			Sensors(this.transform);
+		}
+
+		void Sensors(Transform car)
+		{
+			RaycastHit hit;
+			Vector3 sensor_pos;
+
+			sensor_pos = car.position;
+			sensor_pos += car.forward * HALF_HEIGTH_CAR;
+
+			//Front
+			if (Physics.Raycast(sensor_pos, car.transform.forward, out hit, sensor_length))
+			{
+				if (show_sensors)
+				{
+					Debug.DrawLine(sensor_pos, hit.point, Color.red);
+                    
+				}
+			}else{
+				movement.goStraigthOn();
+			}
+
+			sensor_pos += car.right * HALF_WIDTH_CAR;
+			//Right
+			if (Physics.Raycast(sensor_pos,
+								Quaternion.AngleAxis(angle_sensors, car.transform.up) * car.transform.forward, out hit, sensor_length))
+			{
+				movement.turnLeft(); 
+				if (show_sensors)
+				{
+					Debug.DrawLine(sensor_pos, hit.point, Color.yellow);
+				}
+			}
+
+			sensor_pos += car.right * (-2f * HALF_WIDTH_CAR);
+			//Left
+			if (Physics.Raycast(sensor_pos,
+								Quaternion.AngleAxis(-angle_sensors, car.transform.up) * car.transform.forward, out hit, sensor_length))
+			{
+				movement.turnRight();
+				if (show_sensors)
+				{
+					Debug.DrawLine(sensor_pos, hit.point, Color.blue);
+				}
+			}
+		}
 	}
-
-	void Sensors(Transform car)
-	{
-		RaycastHit hit;
-		Vector3 sensor_pos; 
-
-		sensor_pos = car.position;
-		sensor_pos += car.forward * HALF_HEIGTH_CAR; 
-
-		//Front
-		if (Physics.Raycast(sensor_pos, car.transform.forward, out hit, sensor_length))
-		{
-			if (show_sensors)
-			{
-				Debug.DrawLine(sensor_pos, hit.point, Color.red);
-			}
-		}
-
-		sensor_pos += car.right * HALF_WIDTH_CAR; 
-		//Right
-		if (Physics.Raycast(sensor_pos, 
-		                    Quaternion.AngleAxis(angle_sensors, car.transform.up) * car.transform.forward, out hit, sensor_length) )
-		{
-			if (show_sensors)
-			{
-				Debug.DrawLine(sensor_pos, hit.point, Color.yellow);
-			}
-		}
-
-		sensor_pos += car.right * (-2f * HALF_WIDTH_CAR ); 
-		//Left
-		if (Physics.Raycast(sensor_pos, 
-		                    Quaternion.AngleAxis(-angle_sensors, car.transform.up) * car.transform.forward, out hit, sensor_length))
-		{
-			if (show_sensors)
-			{
-				Debug.DrawLine(sensor_pos, hit.point, Color.blue);
-			}
-		}
-	}
-
 }
--- IAGame/Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
+++ IAGame/Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
@@ -20,8 +20,8 @@ namespace UnityStandardAssets.Vehicles.Car
         private void FixedUpdate()
         {
             // pass the input to the car!
-            float h = CrossPlatformInputManager.GetAxis("Horizontal");
-            float v = CrossPlatformInputManager.GetAxis("Vertical");
+            float h = CrossPlatformInputManager.GetAxis("Horizontal"); //-1 a 1
+            float v = CrossPlatformInputManager.GetAxis("Vertical"); //-1 a 1
 #if !MOBILE_INPUT
             float handbrake = CrossPlatformInputManager.GetAxis("Jump");
             m_Car.Move(h, v, v, handbrake);
